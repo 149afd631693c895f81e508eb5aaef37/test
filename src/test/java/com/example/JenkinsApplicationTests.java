034da327a358aefa69db89e86fbf20703edc0431
hao <hao.pang.slab@gmail.com>
2023-03-28 17:49:35 +0900

    first commit

---
+++ src/test/java/com/example/JenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(true, true);
+		
+	}
+
+}
